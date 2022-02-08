c1dcc0e360e949255b1e1c890db4c6c7f4ac1a7a
Rofidajamal <99247792+Rofidajamal@users.noreply.github.com>
2022-02-08 13:55:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+"""
+1) Data
+• The attached dataset "heart.csv" contains 303 records of patients who 
+have heart disease or don’t have heart disease according to the 13 features 
+in it.
+• The detailed description of the dataset is attached in 
+“heart_dataset_description.pdf”.
+
+2) Requirements:
+Write a python program in which you:
+a- Use these 4 features (trestbps, chol, thalach & oldpeak) as input and the 
+output to be predicted is the “target”.
+b- Split the dataset into training and testing sets.
+c- Use sklearn’s logistic regression to build a model that fits the training 
+data. Then, use that model to predict the output of the test data and print 
+the error of the model.
+d- Implement logistic regression (from scratch) using gradient descent to 
+optimize the parameters of the hypothesis function.
+e- Use the optimized hypothesis function to make predictions on new data.
+f- Calculate the cost (error) in every iteration to see how the error of the 
+hypothesis function changes with every iteration of gradient descent.
+g- Plot the cost against the number of iterations.
+
+"""
 
 # Importing libraries
 import numpy as np
